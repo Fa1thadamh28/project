f22b1bf054e60d457eb1911835ce380f2f5ab191
Jerry Mouse <jerry@tutorialspoint.com>
2019-08-22 18:30:17 +0000

    commit Fibonacci

---
+++ FibonacciExample1.java
@@ -0,0 +1,15 @@
+class FibonacciExample1{
+public static void main(String args[])
+{  
+ int x1=0,x2=1,x3,i,count=10;  
+ System.out.print(x1+" "+x2);//printing 0 and 1  
+  
+ for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed  
+ {  
+  n3=n1+n2;  
+  System.out.print(" "+x3);  
+  x1=x2;  
+  x2=x3;  
+ }  
+
+
