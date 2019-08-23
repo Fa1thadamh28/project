9edb944d6c3efe650cc91e43004229bbba4d1186
Tom Cat <tom@tutorialspoint.com>
2019-08-23 21:53:16 +0000

    commit Fibonacci 3

--- FibonacciExample1.java
+++ FibonacciExample1.java
@@ -6,7 +6,7 @@ public static void main(String args[])
   
  for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed  
  {  
-  x3=x1+x2;  
+  m3=m1+m2;  
   System.out.print(" "+x3);  
   x1=x2;  
   x2=x3;  
