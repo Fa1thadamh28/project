70fcf7d6408c8d976a04f342ce60c89170f3a91d
Jerry Mouse <jerry@tutorialspoint.com>
2019-09-01 03:11:56 +0000

    18 Patch Operation - last Basics

--- src/FibonacciExample2.java
+++ src/FibonacciExample2.java
@@ -1,12 +1,13 @@
 class FibonacciExample1{
 public static void main(String args[])
 {  
- int x1=0,x2=1,x3,i,count=20;  
+// for 18 Git Patch operation
+ int x1=0,x2=1,x3,i,count=10;  
  System.out.print(x1+" "+x2);//printing 0 and 1  
   
  for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed  
  {  
-  m3=m1+m2;  
+  x3=x1+x2;  
   System.out.print(" "+x3);  
   x1=x2;  
   x2=x3;  
